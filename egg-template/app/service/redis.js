f6ffa1e7fedf86ec7fa5e6d2e34707a5c17f282f
supercll <1033581609@qq.com>
2023-07-24 10:05:49 +0800

    feat: egg-template

---
+++ egg-template/app/service/redis.js
@@ -0,0 +1,16 @@
+'use strict';
+const Redis = require('ioredis')
+
+const redis = new Redis(6379, '192.168.0.106', { password: 'root' })
+redis.on('error', err => {
+  if (err) {
+    console.log('Redis 链接错误');
+    redis.quit()
+  }
+})
+
+redis.on('ready', () => {
+  console.log('redis链接成功');
+})
+
+module.exports.redis = redis
